4d9105bd699d5da19a0184001eb637ec251fe45f
KuleshaDmytro <kulesha8@gmail.com>
2026-04-30 13:52:50 +0200

    chore: update configs, dependencies and global styles

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,23 +1,22 @@
 "use client"
-import { SessionProvider } from "next-auth/react"
-
+import { SessionProvider, signOut, useSession } from "next-auth/react"
 import { ApolloProvider } from "@apollo/client";
 import client from "@/app/lib/apolloClient";
-
 import { ThemeProvider } from "@mui/material";
 import theme from "../styles/theme";
 import ErrorProvider from "./ErrorProvider";
 
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-        <SessionProvider>
-          <ApolloProvider client={client}>
-            <ErrorProvider>
-              <ThemeProvider theme={theme}>
-                {children}
-              </ThemeProvider>
-            </ErrorProvider>
-          </ApolloProvider>
-        </SessionProvider>
+    <SessionProvider>
+      <ApolloProvider client={client}>
+        <ErrorProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
+        </ErrorProvider>
+      </ApolloProvider>
+    </SessionProvider>
   )
 }
